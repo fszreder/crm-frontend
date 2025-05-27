b05f7293de3743961b0fafdf1cc85b60d3972e19
Srippo <filip.szreder.slupsk@gmail.com>
2025-05-27 13:05:27 +0200

    little fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './homepage';
 import ClientsPage from '@/pages/ClientsPage';
+import { Login } from './pages/Login';
 
 function App() {
-  return (
-    <div className="p-8">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/clients" element={<ClientsPage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="p-8">
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/homepage" element={<HomePage />} />
+                <Route path="/clients" element={<ClientsPage />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
