34aa4fe782c655f4235e90a6535e539d55cabab3
Srippo <filip.szreder.slupsk@gmail.com>
2025-06-09 14:47:34 +0200

    modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         viteStaticCopy({
             targets: [
                 {
-                    src: path.resolve(__dirname, 'public/_redirects'),
+                    src: 'public/_redirects',
                     dest: '.',
                 },
             ],
