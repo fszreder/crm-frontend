b7aa09320667a0ee81df44dc7e9d5c2e2693b141
Srippo <filip.szreder.slupsk@gmail.com>
2025-06-09 14:10:59 +0200

    Force copy _redirects to dist via vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: path.resolve(__dirname, 'public/_redirects'),
+                    dest: '.',
+                },
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
-})
+});
